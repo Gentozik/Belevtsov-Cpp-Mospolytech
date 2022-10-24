6278147766eeb5a58a3476b6a1f880247dc42ca1
Belevtsov <gentoz@inbox.ru>
2022-10-24 18:13:37 +0300

    Understanding lab 6

--- Programmirovanie/Lab_6/Year.hpp
+++ Programmirovanie/Lab_6/Year.hpp
@@ -1,19 +1,37 @@
 #include <iostream>
-using namespace std;
-using std::cout;
-using std::endl;
+
+enum PlayerCode {
+	NEGATIVE,
+	BIG,
+	ZERO
+};
+
+class PlayerException: public std::exception {
+	PlayerCode code;
+public:
+	PlayerException(PlayerCode code) : code(code) {}
+	const char* what() const throw() {
+		if (code == PlayerCode::NEGATIVE) return "Health < 0";
+		else if (code == PlayerCode::BIG) return "Health >100";
+		else if (code == PlayerCode::ZERO) return "Health = 0";
+	}
+};
 
 class Player {
-private:
-    int health;
+	int health;
 public:
-    Player(int health) {
-      if (health < 0) throw logic_error("Health < 0");
-      else if (health > 100) throw range_error("Health > 100");
-      else if (health == 0) throw exception();
-      this->health = health;
-    }
-    int getHealth() {
-        return health;
-    }
+	Player(int health) : health(health) {
+		if (health < 0) throw PlayerException(PlayerCode::NEGATIVE);
+		else if (health > 100) throw PlayerException(PlayerCode::BIG);
+		else if (health == 0) throw PlayerException(PlayerCode::ZERO);
+	}
+	int getHealth() {
+		return this->health;
+	}
+	void setHealth(int health) {
+		if (health < 0) throw PlayerException(PlayerCode::NEGATIVE);
+		else if (health > 100) throw PlayerException(PlayerCode::BIG);
+		else if (health == 0) throw PlayerException(PlayerCode::ZERO);
+		this->health = health;
+	}
 };
