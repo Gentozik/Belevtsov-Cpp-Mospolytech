288aab5206e85a44eaa224ab50aef6e320763cda
Belevtsov <gentoz@inbox.ru>
2022-10-31 19:25:41 +0300

    lab 7

--- Programmirovanie/Lab_7/Zad_3/dfg.cpp
+++ Programmirovanie/Lab_7/Zad_3/dfg.cpp
@@ -1,3 +1,5 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -6,23 +8,43 @@ using namespace std;
 
 int main()
 {
-    fstream fs;
-    fs.open("a.txt", ios::out);
-    fs << "My table\nMy window";
+  double radius;
+  cout << "Enter radius: ";
+  cin >> radius;
+  double S = trunc(2 * M_PI * radius * 1000) / 1000;
+  string s = to_string(S);
+
+  fstream fs;
+    fs.open("Math.txt", ios::out);
+    fs << s;
     fs.close();
 
-    fs.open("a.txt", ios::in);
-    string data;
-    fs >> data;
-    cout << data<<endl;
-    fs.close();
-    fs.open("a.txt", ios::in);
-    string line;
-    data = "";
-    while (getline(fs, line)) {
-        data += line+"\n";
-    }
-    cout << data << endl;
-    fs.close();
+    fs.open("Math.txt", ios::out);
+    fs << s;
+  	fs.close();
 
+  string add;
+  string show;
+  while (true){
+    cin >> add;
+      if (add == "read"){
+        fs.open("Math.txt", ios::in | ios::app);
+      	fs >> show;
+      	std::cout << show << endl;
+      	fs.close();
+      	continue;
+      }
+      if (add == "erase"){
+      	fs.open("Math.txt", ios::trunc);
+        fs << "";
+      	fs.close();
+        continue;
+      }
+      if (add == "exit"){
+      	break;
+      }
+      fs.open("Math.txt", ios::out | ios::app);
+      fs << add;
+      fs.close();
+  }
 }
